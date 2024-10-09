8433af4ded683e4d453d0553900c670bfa44f450
Maksim <masyanya.maksimka@mail.ru>
2024-10-09 10:22:57 +0300

    create request

--- app/Http/Controllers/Admin/Category/StoreController.php
+++ app/Http/Controllers/Admin/Category/StoreController.php
@@ -3,19 +3,15 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\StoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'name' => 'string',
-            'description' => 'string',
-            'price' => ['required', 'numeric', 'between:0,99999.99'],
-            'duration' => 'integer',
-        ]);
+        $data = $request->validated();
 
         Category::create($data);
         return redirect()->route('category.index');
--- app/Http/Controllers/Admin/Category/UpdateController.php
+++ app/Http/Controllers/Admin/Category/UpdateController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\Category\UpdateRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Category $category)
+    public function __invoke(UpdateRequest $request,Category $category)
     {
-        $data = request()->validate([
-            'name' => 'string',
-            'description' => 'string',
-            'price' => ['required', 'numeric', 'between:0,99999.99'],
-            'duration' => 'integer',
-        ]);
+        $data = $request->validated();
+
         $category->update($data);
         return redirect()->route('category.show', $category->id);
     }
