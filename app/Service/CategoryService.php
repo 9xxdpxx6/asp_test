4702de513a24ce9f27a3efbe753e325564f23351
Maksim <masyanya.maksimka@mail.ru>
2025-02-01 10:38:12 +0300

    Сохранение изображений на сервере, отображение через ссылки

--- app/Service/CategoryService.php
+++ app/Service/CategoryService.php
@@ -7,46 +7,65 @@ use App\Models\CategoryImage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-use mysql_xdevapi\Exception;
 
 class CategoryService
 {
     public function store($data)
     {
-
         try {
             DB::beginTransaction();
-
             // Получаем HTML-контент из поля content
             $htmlContent = $data['description'];
-
             // Используем DOMDocument для парсинга HTML
             $dom = new \DOMDocument();
             libxml_use_internal_errors(true);
             $dom->loadHTML($htmlContent, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
             libxml_clear_errors();  // Очистить ошибки после загрузки
+            // Получаем все теги <img>
+            $images = $dom->getElementsByTagName('img');
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
 
-            $image = $dom->getElementsByTagName('img')->item(0);
+                if (preg_match('/^data:image\/(\w+);base64,/', $src, $type)) {
+                    // Определяем расширение изображения
+                    $extension = strtolower($type[1]);
+                    // Убираем base64 и декодируем изображение
+                    $imageData = substr($src, strpos($src, ',') + 1);
+                    $imageData = base64_decode($imageData);
+                    // Генерируем уникальное имя файла
+                    $fileName = 'image_' . time() . '_' . Str::random(10) . '.' . $extension;
+                    $filePath = 'images/categories/' . $fileName;
+
+                    Storage::disk('public')->put($filePath, $imageData);
+
+                    $imageUrl = url('storage/' . $filePath);
+
+                    // Заменяем src в теге <img> на URL
+                    $img->setAttribute('src', $imageUrl);
+                }
+                $updatedHtmlContent = $dom->saveHTML();
+            }
 
             $category = Category::create([
                 'name' => $data['name'],
                 'slug' => $data['slug'],
                 'icon' => $data['icon'],
-                'description' => $htmlContent,
+                'description' => $updatedHtmlContent,
                 'price' => $data['price'],
                 'duration' => $data['duration'],
             ]);
 
             DB::commit();
 
-        } catch (\Exception $e) {
+
+        } catch
+        (\Exception $e) {
             DB::rollBack();
             abort(500);
-
         }
     }
 
-    public function update($data,Category $category)
+    public function update($data, Category $category)
     {
 
         try {
@@ -57,7 +76,6 @@ class CategoryService
             }
 
             $htmlContent = $data['description'];
-
             // Используем DOMDocument для парсинга HTML
             $dom = new \DOMDocument();
             libxml_use_internal_errors(true);
@@ -65,9 +83,72 @@ class CategoryService
             libxml_clear_errors();  // Очистить ошибки после загрузки
             $category = Category::findOrFail($category->id);
 
-            if($category->icon){
+            if ($category->icon) {
                 Storage::disk('public')->delete($category->icon);
             }
+            // Находим текущие изображения в описании
+            $currentImages = [];
+            $currentDom = new \DOMDocument();
+
+            if ($category->description) {
+                libxml_use_internal_errors(true);
+                $currentDom->loadHTML($category->description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+                libxml_clear_errors();
+
+                $currentImageTags = $currentDom->getElementsByTagName('img');
+                foreach ($currentImageTags as $currentImg) {
+                    $currentImages[] = $currentImg->getAttribute('src');
+                }
+            }
+
+            // Проверяем наличие изображений в новом контенте
+            $newDom = new \DOMDocument();
+            libxml_use_internal_errors(true);
+            $newDom->loadHTML($htmlContent, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+            libxml_clear_errors();
+
+            $newImageTags = $newDom->getElementsByTagName('img');
+            $newImages = [];
+
+            foreach ($newImageTags as $newImg) {
+                $newImages[] = $newImg->getAttribute('src');
+            }
+// Удаляем старые изображения и их ссылки только если они не присутствуют в новом контенте
+            foreach ($currentImages as $oldImage) {
+                // Проверяем, есть ли это изображение в новом контенте
+                if (!in_array($oldImage, $newImages)) {
+                    // Удаляем изображение с сервера
+                    $path = str_replace(url('storage/'), '', $oldImage);
+                    Storage::disk('public')->delete($path);
+                }
+            }
+
+
+            // Обработка новых изображений
+            $images = $dom->getElementsByTagName('img');
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+
+                if (preg_match('/^data:image\/(\w+);base64,/', $src, $type)) {
+                    // Определяем расширение изображения
+                    $extension = strtolower($type[1]);
+                    // Убираем base64 и декодируем изображение
+                    $imageData = substr($src, strpos($src, ',') + 1);
+                    $imageData = base64_decode($imageData);
+                    // Генерируем уникальное имя файла
+                    $fileName = 'image_' . time() . '_' . Str::random(10) . '.' . $extension;
+                    $filePath = 'images/categories/' . $fileName;
+
+                    Storage::disk('public')->put($filePath, $imageData);
+
+                    $imageUrl = url('storage/' . $filePath);
+
+                    // Заменяем src в теге <img> на URL
+                    $img->setAttribute('src', $imageUrl);
+                }
+                $htmlContent = $dom->saveHTML();
+            }
+
             $category->update([
                 'name' => $data['name'],
                 'slug' => $data['slug'],
@@ -78,6 +159,7 @@ class CategoryService
             ]);
 
             DB::commit();
+
         } catch (\Exception $e) {
             DB::rollBack();
             abort(500);
@@ -89,8 +171,29 @@ class CategoryService
         try {
             DB::beginTransaction();
 
-            if($category->preview_path){
+            if ($category->preview_path) {
                 Storage::disk('public')->delete($category->preview_path);
+            }
+            // Находим текущие изображения в описании
+            $currentImages = [];
+            $currentDom = new \DOMDocument();
+
+            if ($category->description) {
+                libxml_use_internal_errors(true);
+                $currentDom->loadHTML($category->description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+                libxml_clear_errors();
+
+                $currentImageTags = $currentDom->getElementsByTagName('img');
+                foreach ($currentImageTags as $currentImg) {
+                    $currentImages[] = $currentImg->getAttribute('src');
+                }
+            }
+
+// Удаляем старые изображения и их ссылки
+            foreach ($currentImages as $oldImage) {
+                // Проверяем, есть ли это изображение в новом контенте
+                $path = str_replace(url('storage/'), '', $oldImage);
+                Storage::disk('public')->delete($path);
             }
             $category->delete();
 
@@ -100,4 +203,6 @@ class CategoryService
             abort(500);
         }
     }
+
+
 }
